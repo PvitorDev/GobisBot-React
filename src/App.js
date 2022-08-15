2b44b7b5e69f493225c742efaa5f8eada6c1a5b7
PvitorDev <pvitor.dev@gmail.com>
2022-08-15 03:17:48 -0300

    Style v1

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import Card from "./components/Card";
 import Social from "./components/Social";
 function App() {
   return (
-    <div>
+    <div className="ok">
+      <Social />
       <div className="header">
         <h1>Rádio Gobis BOT</h1>
       </div>
       <div className="cards">
         <Card />
       </div>
-
-      <Social />
     </div>
   );
 }
